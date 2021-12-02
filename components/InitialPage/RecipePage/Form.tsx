59f3b2ede32889543c7f0096f18161be6356bf24
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-12-03 00:02:26 +0100

    dd

--- components/InitialPage/RecipePage/Form.tsx
+++ components/InitialPage/RecipePage/Form.tsx
@@ -2,9 +2,35 @@ import { Button, Input } from "@/styles/customComponents";
 import styled from "styled-components";
 
 const Form = () => {
+	// 	var search = document.querySelector('#search');
+	// var results = document.querySelector('#suggestions');
+	// var templateContent = document.querySelector('#resultstemplate').content;
+	// search.addEventListener('keyup', function handler(event) {
+	//     while (results.children.length) results.removeChild(results.firstChild);
+	//     var inputVal = new RegExp(search.value.trim(), 'i');
+	//     var set = Array.prototype.reduce.call(templateContent.cloneNode(true).children, function searchFilter(frag, item, i) {
+	//         if (inputVal.test(item.textContent) && frag.children.length < 6) frag.appendChild(item);
+	//         return frag;
+	//     }, document.createDocumentFragment());
+	//     results.appendChild(set);
+	// });
 	return (
 		<SForm>
-			<Input type="search" placeholder="najdi recept podle názvu" />
+			<Input
+				type="search"
+				list="suggestions"
+				placeholder="najdi recept podle názvu"
+				id="search"
+				minLength={3}
+			/>
+			<datalist id="suggestions">
+				<option>mercedes</option>
+				<option>audi</option>
+				<option>mazda1</option>
+				<option>mazda2</option>
+				<option>mazda3</option>
+				<option>mazda4</option>
+			</datalist>
 			<Button>hledej</Button>
 		</SForm>
 	);
