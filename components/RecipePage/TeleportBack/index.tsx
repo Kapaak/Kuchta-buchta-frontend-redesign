ab9a050ce1aad0ba6b43d8fd97c87d26aefee536
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-12-03 10:49:51 +0100

    dd

--- components/RecipePage/TeleportBack/index.tsx
+++ components/RecipePage/TeleportBack/index.tsx
@@ -1,22 +1,25 @@
 //libraries
 import styled from "styled-components";
 import { useRouter } from "next/router";
+import Div100vh from "react-div-100vh";
 import Image from "next/image";
 
 const TeleportBack = () => {
 	const router = useRouter();
 	return (
-		<STeleportBack onClick={() => router.push("/")}>
-			<Image src="/icons/circle-icon.svg" width={30} height={30} />
-		</STeleportBack>
+		<Div100vh>
+			<STeleportBack onClick={() => router.push("/")}>
+				<Image src="/icons/circle-icon.svg" width={30} height={30} />
+			</STeleportBack>
+		</Div100vh>
 	);
 };
 
 const STeleportBack = styled.div`
 	position: fixed;
 	top: 0rem;
+	height: 100%;
 	width: 100%;
-	height: 100vh;
 	pointer-events: none;
 
 	& > span {
